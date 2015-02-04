39ca3821c6bd129315d4863cea26aec760088768
Nicolas <nbonniot@compagnie-hyperactive.com>
2015-02-04 17:48:34 +0100

    - Use Wordpress dedicated function for getting plugins path

--- post-type-manager.php
+++ post-type-manager.php
@@ -18,16 +18,16 @@ function ptm_register_js($hook) {
 
 add_action( 'admin_enqueue_scripts', 'ptm_register_js' );
 
-require_once(dirname(__FILE__).'/classes/post-type-meta-box.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-abstract.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-text.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-textarea.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-checkbox.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-select.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-radio.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-post-list.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-image.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-editor.php');
-require_once(dirname(__FILE__).'/classes/fields/custom-field-address.php');
-require_once(dirname(__FILE__).'/classes/post-type.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/post-type-meta-box.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-abstract.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-text.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-textarea.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-checkbox.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-select.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-radio.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-post-list.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-image.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-editor.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/fields/custom-field-address.php');
+require_once(plugin_dir_path( __FILE__ ).'classes/post-type.php');
 
